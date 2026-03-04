b2d73b230ae483f681704dd668d0c214a2205074
Tuhin S <tuhin.eios@gmail.com>
2026-03-05 00:01:16 +0530

    Two Sum

--- Easy/Two_Sum_Easy.playground/Contents.swift
+++ Easy/Two_Sum_Easy.playground/Contents.swift
@@ -1,9 +1,21 @@
-import UIKit
+import Foundation
 
 // Link - https://leetcode.com/problems/two-sum/description/
 
 func twoSum(of arr: [Int], target trgt: Int) {
-    if arr.count < 2 { return }
-    
+    if arr.count < 2 { print("The source data is not in correct format"); return }
+    var dictData: [Int: Int] = [:]
+    for (index, value) in arr.enumerated() {
+        print(index, value)
+        let diff: Int = (trgt - value)
+        if let storedValue = dictData[diff] {
+            print("Two Sum is - [\(storedValue), \(index)]")
+            break
+        } else {
+            dictData[value] = index
+        }
+    }
     
 }
+
+twoSum(of: [2, 11, 15, 7], target: 9)
